3d217a101eb6d27c07c399bc135c98aa9bc55a25
Kevin <150013294+KevinHogendoorn@users.noreply.github.com>
2024-04-16 12:25:55 +0200

    Commit con los TestCreados

---
+++ examen/src/MoneyTest.java
@@ -0,0 +1,50 @@
+import jdk.jfr.StackTrace;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MoneyTest {
+    @Test
+    void cambioEUR_USD(){
+
+    }
+
+    @Test
+    void cambioGBP_EUR(){
+
+    }
+
+    @Test
+    void cambioEUR_GBP(){
+
+    }
+
+    @Test
+    void cambioUSD_EUR(){
+
+    }
+
+    @Test
+    void cambioGBP_USD(){
+
+    }
+
+    @Test
+    void cambioUSD_GBP(){
+
+    }
+    @Test
+    void cambioPTS_EUR(){
+
+    }
+
+    @Test
+    void cambioEUR_PTS(){
+
+    }
+
+    @Test
+    void cambioUSD_EUR2(){
+
+    }
+}
